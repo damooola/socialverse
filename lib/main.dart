e0d86adf0ee68214d71f026e911ad2b7a91ae009
damooola <damolanelson@gmail.com>
2024-09-26 00:34:44 +0100

    add routes

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,24 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:social_media_app/auth/auth_gate.dart';
+import 'package:social_media_app/auth/signin_or_signup_page.dart';
 import 'package:social_media_app/firebase_options.dart';
+import 'package:social_media_app/pages/home_page.dart';
+import 'package:social_media_app/pages/profile_page.dart';
+import 'package:social_media_app/pages/users_page.dart';
 import 'package:social_media_app/theme/dark_mode.dart';
 import 'package:social_media_app/theme/light_mode.dart';
 import 'package:social_media_app/theme/theme_provider.dart';
 
 void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
+
+  // initialise firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
+
+// add provider to app
   runApp(ChangeNotifierProvider(
       create: (context) => ThemeProvider(), child: const MyApp()));
 }
@@ -25,6 +34,12 @@ class MyApp extends StatelessWidget {
       theme: lightMode,
       darkTheme: darkMode,
       home: const AuthGate(),
+      routes: {
+        "/signin_signup_page": (context) => const SigninOrSignupPage(),
+        "/home_page": (context) => const HomePage(),
+        "/profile_page": (context) => const ProfilePage(),
+        "/users_page": (context) => const UsersPage(),
+      },
     );
   }
 }
