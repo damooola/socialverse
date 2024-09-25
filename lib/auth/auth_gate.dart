cae9fe0e9c5b1e1c4d3494040636c3a8767a5d81
damooola <damolanelson@gmail.com>
2024-09-26 00:35:53 +0100

    check if user is logged in

--- lib/auth/auth_gate.dart
+++ lib/auth/auth_gate.dart
@@ -13,9 +13,12 @@ class AuthGate extends StatelessWidget {
       body: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
+          //user is logged in
           if (snapshot.hasData) {
             return const HomePage();
-          } else {
+          }
+          //user is not logged in
+          else {
             return const SigninOrSignupPage();
           }
         },
