597e27566ef37ce5aedb60fd574708c2beb41da6
damooola <damolanelson@gmail.com>
2024-09-26 00:31:37 +0100

    drawer tile in the drawer

---
+++ lib/components/my_drawer_tile.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MyDrawerTile extends StatelessWidget {
+  final String text;
+  final IconData iconData;
+  final void Function()? onTap;
+  const MyDrawerTile(
+      {super.key,
+      required this.text,
+      required this.iconData,
+      required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      padding: const EdgeInsets.only(left: 20),
+      child: ListTile(
+        title: Text(
+          text,
+          style: TextStyle(
+              color: Theme.of(context).colorScheme.inversePrimary,
+              letterSpacing: 5),
+        ),
+        leading: Icon(
+          iconData,
+          color: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        onTap: onTap,
+      ),
+    );
+  }
+}
