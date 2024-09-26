156cdfefed67d8168574a94fc6b2e74ed7dd4384
damooola <damolanelson@gmail.com>
2024-09-26 11:16:33 +0100

    build user profile with data from users collection

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,13 +1,62 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:social_media_app/components/my_app_bar.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  ProfilePage({super.key});
+
+// current logged in user
+  final User? currentUser = FirebaseAuth.instance.currentUser;
+
+  //fetch user details
+  Future<DocumentSnapshot<Map<String, dynamic>>> fetchUserDetails() async {
+    return await FirebaseFirestore.instance
+        .collection("Users")
+        .doc(currentUser!.email)
+        .get();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: MyAppBar(title: "Profile Page"),
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: const MyAppBar(title: "Profile Page"),
+      body: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+        future: fetchUserDetails(),
+        builder: (context, snapshot) {
+          // loading circle
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          // error handling
+          else if (snapshot.hasError) {
+            return const Center(
+              child: Text("Error.."),
+            );
+          }
+
+          // if avalable data exists
+          else if (snapshot.hasData) {
+            // get data
+            Map<String, dynamic>? userData = snapshot.data!.data();
+
+            return Center(
+              child: Column(
+                children: [
+                  Text(userData!["email"]),
+                  Text(userData["username"])
+                ],
+              ),
+            );
+          } else {
+            return Container();
+          }
+        },
+      ),
     );
   }
 }
