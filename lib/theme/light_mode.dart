255038d76874354d0a2775ae2bc6105d414fc800
damooola <damolanelson@gmail.com>
2024-09-26 21:29:38 +0100

    edit colours

--- lib/theme/light_mode.dart
+++ lib/theme/light_mode.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 ThemeData lightMode = ThemeData(
     brightness: Brightness.light,
     colorScheme: ColorScheme.light(
-      surface: Colors.grey.shade100,
-      primary: Colors.grey.shade300,
-      secondary: Colors.grey.shade500,
+      surface: Colors.grey.shade50,
+      primary: Colors.grey.shade400,
+      secondary: Colors.grey.shade300,
       tertiary: Colors.white,
       inversePrimary: Colors.grey.shade800,
     ),
